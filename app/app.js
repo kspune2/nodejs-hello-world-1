d79a3bf55d294edd14efd70b11c4935ddff70628
Admin Admin <admin@admin.com>
2019-08-27 13:56:47 +0000

    test

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! from eclipse CHE');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
